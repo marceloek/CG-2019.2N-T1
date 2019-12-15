bd8c65a8ef37c8ff1416d5443347a2812b82bc9b
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-12-14 22:21:06 -0300

    add ceu e controles min max

--- src/cenario_pokemon/cenario_pokemon.js
+++ src/cenario_pokemon/cenario_pokemon.js
@@ -1,4 +1,5 @@
 var scene, camera;
+var loader = new THREE.TextureLoader();
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -6,26 +7,27 @@ document.body.appendChild( renderer.domElement );
 
 // cena
 scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xcce0ff );
-scene.fog = new THREE.Fog( 0xcce0ff, 5, 12000 );
+
+// ceu
+var skyTexture = loader.load( '../../textures/sky/cloud-21.jpg' );
+scene.background = skyTexture;
+scene.fog = new THREE.Fog( 0xadc8f0, 5, 12000 );
 
 // camera
 camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
-camera.position.set( 1000, 50, 1500 );
+camera.position.set( 1000, 250, 1500 );
 
 // luz
 scene.add( new THREE.AmbientLight( 0x666666 ) );
 
-var light = new THREE.DirectionalLight( 0xdfebff, 1 );
+var light = new THREE.DirectionalLight( 0xffffff, 0.8 );
 light.position.set( 50, 200, 100 );
 light.position.multiplyScalar( 1.3 );
 
 scene.add( light );
 
 // chao
-var loader = new THREE.TextureLoader();
-
-var groundTexture = loader.load( '../../textures/grasslight-big.jpg' );
+var groundTexture = loader.load( '../../textures/terrain/grasslight-big.jpg' );
 groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
 groundTexture.repeat.set( 25, 25 );
 groundTexture.anisotropy = 16;
@@ -41,6 +43,8 @@ scene.add( mesh );
 
 // controles
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.minDistance = 1000;
+controls.maxDistance = 5000;
             
 var animate = function () {
     requestAnimationFrame(animate);
